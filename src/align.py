598d84ebd6738f4ea855a5d4385e7096686a4031
Brono25 <brono_25@hotmail.com>
2024-05-10 14:50:16 +1000

    phoneme mapping working

--- src/align.py
+++ src/align.py
@@ -1,21 +1,25 @@
-# Scoring scheme
-LEFT = "←"
-UP = "↑"
-DIAG = "↖"
-
+from pretty import pretty_matrices, pretty_sequences
+from phoneme_scoring import create_phoneme_similarity_matrix
 
 class NeedlemanWunsch:
 
-    def __init__(self, seq1, seq2, gap_penalty=1):
+    def __init__(self, gap_penalty=4):
+        self.LEFT = "←"
+        self.UP = "↑"
+        self.DIAG = "↖"
+        self.DONE = "X"
+        self.GAP = "-"
         self.gap_penalty = abs(gap_penalty)
-        seq1.insert(0, "") #add padding to start of sequences
-        seq2.insert(0, "")
-        self.row_seq = seq1
-        self.col_seq = seq2
-        self.N_row = len(self.row_seq)
-        self.N_col = len(self.col_seq)
+        self.similarity_matrix = create_phoneme_similarity_matrix()
+
+    def __call__(self, seq1, seq2):
+        self.top_seq = [""] + seq1
+        self.left_seq = [""] + seq2
+        self.N_row = len(self.top_seq)
+        self.N_col = len(self.left_seq)
         self.score_matrix, self.trace_matrix = self._init_score_and_trace_matrix()
         self._fill_score_matrix()
+        return self._traceback()
 
     def _init_score_and_trace_matrix(self):
         score_matrix = [[0] * self.N_col for _ in range(self.N_row)]
@@ -23,14 +27,14 @@ class NeedlemanWunsch:
         gap = 0
         for i in range(self.N_row):
             score_matrix[i][0] = gap
-            trace_matrix[i][0] = UP
+            trace_matrix[i][0] = self.UP
             gap = gap - self.gap_penalty
         gap = 0
         for j in range(self.N_col):
             score_matrix[0][j] = gap
-            trace_matrix[0][j] = LEFT
+            trace_matrix[0][j] = self.LEFT
             gap = gap - self.gap_penalty
-        trace_matrix[0][0] = "X"
+        trace_matrix[0][0] = self.DONE
         return score_matrix, trace_matrix
 
     def _fill_score_matrix(self):
@@ -40,20 +44,47 @@ class NeedlemanWunsch:
                 self.score_matrix[i][j] = score
                 self.trace_matrix[i][j] = direction
 
-    def _score_cell(self, i, j):
-        row_char = self.row_seq[i]
-        col_char = self.col_seq[j]
+    def _traceback(self):
+        aligned_top_seq = []
+        aligned_left_seq = []
+        i = self.N_row - 1
+        j = self.N_col - 1
+        while i > 0 or j > 0:
+            cell = self.trace_matrix[i][j]
+            if cell == self.DIAG:
+                aligned_top_seq.append(self.top_seq[i])
+                aligned_left_seq.append(self.left_seq[j])
+                i -= 1
+                j -= 1
+            elif cell == self.LEFT:
+                aligned_top_seq.append(self.GAP)
+                aligned_left_seq.append(self.left_seq[j])
+                j -= 1
+            elif cell == self.UP:
+                aligned_top_seq.append(self.top_seq[i])
+                aligned_left_seq.append(self.GAP)
+                i -= 1
+        aligned_top_seq.reverse()
+        aligned_left_seq.reverse()
+        return aligned_top_seq, aligned_left_seq
 
-        q_diag = self.score_matrix[i - 1][j - 1] + self.substitution_score(
-            row_char, col_char
-        )
+    def _score_cell(self, i, j):
+        row_char = self.top_seq[i]
+        col_char = self.left_seq[j]
+        s_ij = self.substitution_score(row_char, col_char)
+        q_diag = self.score_matrix[i - 1][j - 1] + s_ij
         q_up = self.score_matrix[i - 1][j] - self.gap_penalty
         q_left = self.score_matrix[i][j - 1] - self.gap_penalty
-
         max_score, direction = max(
-            (q_diag, DIAG), (q_up, UP), (q_left, LEFT), key=lambda x: x[0]
+            (q_diag, self.DIAG),
+            (q_up, self.UP),
+            (q_left, self.LEFT),
+            key=lambda x: x[0],
         )
         return max_score, direction
+
+    def substitution_score(self, char1, char2):
+        return self.similarity_matrix[char1][char2]
 
     def substitution_score(self, char1, char2):
         if char1 == char2:
@@ -61,42 +92,17 @@ class NeedlemanWunsch:
         return -1
 
     def __str__(self):
-        # Create header for score matrix
-        score_header = (
-            "Score Matrix:\n    "
-            + " ".join(f"{phoneme:>3}" for phoneme in self.col_seq)
-            + "\n"
+        return pretty_matrices(
+            self.top_seq, self.left_seq, self.score_matrix, self.trace_matrix
         )
-        score_rows = ""
-        for i, row_phoneme in enumerate(self.row_seq):
-            row = (
-                f"{row_phoneme:>3} "
-                + " ".join(f"{self.score_matrix[i][j]:>3}" for j in range(self.N_col))
-                + "\n"
-            )
-            score_rows += row
-
-        # Create header for trace matrix
-        trace_header = (
-            "\nTraceback Matrix:\n    "
-            + " ".join(f"{phoneme:>3}" for phoneme in self.col_seq)
-            + "\n"
-        )
-        trace_rows = ""
-        for i, row_phoneme in enumerate(self.row_seq):
-            row = (
-                f"{row_phoneme:>3} "
-                + " ".join(f"{self.trace_matrix[i][j]:>3}" for j in range(self.N_col))
-                + "\n"
-            )
-            trace_rows += row
-
-        return score_header + score_rows + trace_header + trace_rows
 
 
-# Example usage
-seq1 = "C A G C T A".split(" ")
-seq2 = "C A C A T A".split(" ")
-scoring_matrix = NeedlemanWunsch(seq2, seq1)
+if __name__ == "__main__":
+    # Example usage
+    seq1 = "f ih sh".split(" ")
+    seq2 = "f iy".split(" ")
+    scoring_matrix = NeedlemanWunsch()
+    seq1, seq2 = scoring_matrix(seq2, seq1)
 
-print(scoring_matrix)
+    # print(scoring_matrix)
+    pretty_sequences(seq1, seq2)
