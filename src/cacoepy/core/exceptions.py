258a9da5a50a18f36e6923359ce270a0c0139fc8
Brono25 <brono_25@hotmail.com>
2024-05-15 23:49:37 +1000

    added traceback index error

--- src/cacoepy/core/exceptions.py
+++ src/cacoepy/core/exceptions.py
@@ -1,11 +1,16 @@
-
 class ElementNotInVocabError(Exception):
-    """Exception raised when a sequence element is not found in the vocabulary of the substitution matrix."""
+    """Exception raised when a sequence element is not found in the vocabulary of the Similarity matrix."""
     def __init__(self, message):
         super().__init__(message)
 
 
-class InvalidSubstitutionError(Exception):
-    """Exception raised for invalid substitution inputs in NeedlemanWunschConfig."""
+class InvalidSimilarityError(Exception):
+    """Exception raised for invalid similarity inputs in NeedlemanWunschConfig."""
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class TracebackIndexError(Exception):
+    """Exception raised for indexing error during traceback"""
     def __init__(self, message):
         super().__init__(message)
