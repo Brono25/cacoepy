9f51125076069390fc73bcea2a2d762d3ca85d77
Brono25 <brono_25@hotmail.com>
2024-05-18 02:18:04 +1000

    changed the exception name to similarity

---
+++ tests/core/test_Needleman_Wunsch_config.py
@@ -0,0 +1,26 @@
+import pytest
+from cacoepy.core.Needleman_Wunsch import NeedlemanWunschConfig
+from cacoepy.core.exceptions import InvalidSimilarityError
+
+def test_incorrect_substitution_function():
+    def incorrect_substitution_function1(a, b, c):
+        pass
+
+    with pytest.raises(
+        InvalidSimilarityError,
+        match="Substitution function must have only two arguments.",
+    ):
+        NeedlemanWunschConfig(
+            gap_penalty=-4, substitution=incorrect_substitution_function1
+        )
+
+    def incorrect_substitution_function2(a):
+        pass
+
+    with pytest.raises(
+        InvalidSimilarityError,
+        match="Substitution function must have only two arguments.",
+    ):
+        NeedlemanWunschConfig(
+            gap_penalty=-4, substitution=incorrect_substitution_function2
+        )
