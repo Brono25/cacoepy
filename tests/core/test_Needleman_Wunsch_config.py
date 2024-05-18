8e8558f03304907b001887740566f976b28163c4
Brono25 <brono_25@hotmail.com>
2024-05-18 23:05:02 +1000

    renamed substitution to similarity

--- tests/core/test_Needleman_Wunsch_config.py
+++ tests/core/test_Needleman_Wunsch_config.py
@@ -2,25 +2,25 @@ import pytest
 from cacoepy.core.Needleman_Wunsch import NeedlemanWunschConfig
 from cacoepy.core.exceptions import InvalidSimilarityError
 
-def test_incorrect_substitution_function():
-    def incorrect_substitution_function1(a, b, c):
+def test_incorrect_similarity_function():
+    def incorrect_similarity_function1(a, b, c):
         pass
 
     with pytest.raises(
         InvalidSimilarityError,
-        match="Substitution function must have only two arguments.",
+        match="Similarity function must have only two arguments.",
     ):
         NeedlemanWunschConfig(
-            gap_penalty=-4, substitution=incorrect_substitution_function1
+            gap_penalty=-4, similarity=incorrect_similarity_function1
         )
 
-    def incorrect_substitution_function2(a):
+    def incorrect_similarity_function2(a):
         pass
 
     with pytest.raises(
         InvalidSimilarityError,
-        match="Substitution function must have only two arguments.",
+        match="Similarity function must have only two arguments.",
     ):
         NeedlemanWunschConfig(
-            gap_penalty=-4, substitution=incorrect_substitution_function2
+            gap_penalty=-4, similarity=incorrect_similarity_function2
         )
